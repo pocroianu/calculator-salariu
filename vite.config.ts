52a979cd6a8160d351390781f0af59431f2164ee
Geani Pocroianu <nicolaepocroianu@waterford.org>
2026-04-29 15:10:28 +0300

    fix base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/calculator_salariu/',
+  base: '/calculator-salariu/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
